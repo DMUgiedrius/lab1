a4aed64b18b92a828bb99170f911ce917ca4d098
P15191825 <P15191825@GWHXX631WIN002W>
2017-10-04 11:44:32 +0100

    added something copy paste

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -2,10 +2,28 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
 
-
-int main()
+int main(int argc, char* argv[])
 {
+	//variables
+	int guessesRemaining = 5;
+
+	// seed the random number generator
+	srand(time(nullptr));
+
+	int computerRandomNumber = rand() % 100 + 1;
+
+	//game start message
+	std::cout << "Guess a number between 1 and 100";
+
+	// while
+	while (guessesRemaining > 0)
+	{
+
+	}
     return 0;
 }
 
